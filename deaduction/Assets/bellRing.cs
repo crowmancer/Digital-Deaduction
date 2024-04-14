735a5c4ac8cfb33960440b3ca874cffd85182d45
1moremystery <166890492+1moremystery@users.noreply.github.com>
2024-04-14 00:19:34 -0500

    bell

---
+++ deaduction/Assets/bellRing.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bellRing : MonoBehaviour
+{
+    
+    public AudioSource bell;
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("this thing goes gong");
+        bell.Play();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("this thing goes gong but trigger");
+        bell.Play();
+    }
+
+    public void debugSpellout(string spellout)
+    {
+        Debug.Log(spellout);
+    }
+}
